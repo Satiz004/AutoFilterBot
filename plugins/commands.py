485571899af6cc5b3fa7a52cf88acf6e588102c4
Satiz004 <138674478+Satiz004@users.noreply.github.com>
2023-10-14 20:23:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -345,14 +345,14 @@ async def save_shortlink(client, message):
     try:
         _, url, api = message.text.split(" ", 2)
     except:
-        return await message.reply_text("<b>Command Incomplete:-\n\ngive me a shortlink & api along with the command...\n\nEx:- <code>/shortlink mdisklink.link 5843c3cc645f5077b2200a2c77e0344879880b3e</code>")
+        return await message.reply_text("<b>Incomplete:-\n\nContact Developer...\n\nEx:- <code>@moviez_areas</code>")
     
     try:
         shortzy = Shortzy(api_key=api, base_site=url)
         link = f'https://t.me/{temp.U_NAME}'
         await shortzy.convert(link)
     except:
-        return await message.reply_text("Your shortlink API or URL invalid, Please Check again!")
+        return await message.reply_text("Your URL invalid, Please Check again!")
     
     await save_group_settings(grp_id, 'url', url)
     await save_group_settings(grp_id, 'api', api)
