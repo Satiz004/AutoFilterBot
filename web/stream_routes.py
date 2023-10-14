9e354a28434330926ee3f30d3d2e9265cfbcad21
Satiz004 <138674478+Satiz004@users.noreply.github.com>
2023-10-15 02:31:43 +0530

    Update stream_routes.py

--- web/stream_routes.py
+++ web/stream_routes.py
@@ -39,10 +39,10 @@ home_template = """
 </head>
 <body>
     <!-- Image above the chatbot title -->
-    <img src="https://telegra.ph/file/582962dbc60ae9ce722b0.jpg" alt="Chatbot Image" class="chatbot-image">
+    <img src="https://graph.org/file/912c48fcf4669ba63255d.jpg" alt="Chatbot Image" class="chatbot-image">
 
     <!-- Chatbot title -->
-    <h1 class="chatbot-title">SL Bots</h1>
+    <h1 class="chatbot-title">Rolex Bots</h1>
 </body>
 </html>
 """
